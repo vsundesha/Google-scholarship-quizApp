b5081f896f430aa18ac7b5947aa79be3ed4df5de
Vicky Madan Sundesha <me.vickysundesha@gmail.com>
2018-03-03 13:12:18 +0100

    second commit

--- app/src/main/java/com/example/android/quiz/MainActivity.java
+++ app/src/main/java/com/example/android/quiz/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -17,7 +19,9 @@ public class MainActivity extends AppCompatActivity {
     static int totalPoints;
     RadioButton q1a1c;
     RadioButton q2a1c;
-    RadioButton q3a2c;
+    CheckBox q3a1c;
+    CheckBox q3a2c;
+    EditText q4a1c;
 
 
 
@@ -28,19 +32,31 @@ public class MainActivity extends AppCompatActivity {
 
         q1a1c = (RadioButton) findViewById(R.id.q1a1c);
         q2a1c = (RadioButton) findViewById(R.id.q2a1c);
-        q3a2c = (RadioButton) findViewById(R.id.q3a2c);
+        q3a1c = (CheckBox) findViewById(R.id.q3a1c);
+        q3a2c = (CheckBox) findViewById(R.id.q3a2c);
+        q4a1c = (EditText) findViewById(R.id.q4a1c);
+
+
 
     }
 
+    /*
+        On submit counts the points
+     */
     public void submit(View v) {
         totalPoints=0;
+
+        System.out.println(q4a1c.getText().toString());
         if(q1a1c.isChecked()) {
             totalPoints ++;
         }
         if(q2a1c.isChecked()) {
             totalPoints ++;
         }
-        if (q3a2c.isChecked()) {
+        if (q3a2c.isChecked() && q3a1c.isChecked()) {
+            totalPoints ++;
+        }
+        if (q4a1c.getText().toString().toLowerCase().equals("oreo")){
             totalPoints ++;
         }
 
@@ -48,29 +64,29 @@ public class MainActivity extends AppCompatActivity {
         this.score(totalPoints);
 
     }
-
+    /*
+        Sets the score
+        @param totalpoints
+     */
     public void score(int totalPoints){
         Context context = getApplicationContext();
-        String mess = getResources().getString(R.string.toast);
-        CharSequence text = String.format(mess, totalPoints);;
+        String mess;
+        CharSequence text;
         int duration = Toast.LENGTH_SHORT;
 
-
-        if(MainActivity.totalPoints > 0 || MainActivity.totalPoints < 3) {
+        if (totalPoints == 4){
+            mess = getResources().getString(R.string.toast2);
+            text = String.format(mess, totalPoints);
+            Toast.makeText(context,text,duration).show();
+        } else {
+            mess = getResources().getString(R.string.toast);
+            text = String.format(mess, totalPoints);
             Toast.makeText(context, text, duration).show();
-
-
-        } else if (MainActivity.totalPoints == 0) {
-            Toast.makeText(context, text, duration).show();
-
-
-        } else if (MainActivity.totalPoints ==  3){
-            Toast.makeText(context, text, duration).show();
-
-
         }
     }
-
+    /*
+        reset the app and clear all answers
+     */
     public void resetAnswers(View v) {
         totalPoints = 0;
         Intent intent = new Intent(this, MainActivity.class);
